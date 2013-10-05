b11239ef920ea063aac0ae912e4dd29a6a27c18a
mark-keaton <mkeaton@gmail.com>
2013-10-04 23:28:16 -0700

    Committing new changes.

--- project.py
+++ project.py
@@ -7,8 +7,17 @@ def adder(arg1, arg2):
     return arg1 + arg2
 
 
+def subtracter(arg1, arg2):
+    return arg1 - arg2
+
 class Test(TestCase):
     def test_adder(self):
         self.assertEqual(adder(5, 4), 9)
 
+
+
+
+class TestMoreStuff(TestCase):
+    def test_subtracter(self):
+        self.assertEqual(subtracter(9, 2), 7)
 
